d2a7cc06b66c211c095ce03c527057652ecc872b
Raghu Gangolu <rb22csr1p02@student.nitw.ac.in>
2026-02-24 19:44:16 +0530

    Delete rashomon_emotion directory

--- rashomon_emotion/interpretability.py
+++
@@ -1,7 +0,0 @@
-# SHAP and other interpretability methods.
-import shap
-
-def explain_with_shap(model, data):
-    explainer = shap.Explainer(model, data)
-    shap_values = explainer(data)
-    shap.summary_plot(shap_values, data)
